07bfce4de318cfbe8cf65d90ab792e37df04cc31
Julian Dasilva <julianalecssandredasilva@gmail.com>
2020-06-28 18:00:57 -0400

    created example routes.go file inside the api directory that will handle our api routing

---
+++ api/routes.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+//Holds All the Different API Routes and Route Setup Functions
+
+const API_VERSION = "v1"
+
+type LocalRouter struct {
+	*mux.Router
+}
+
+// HandleFuncVersioned is a small wrapper function for Handle Func to append our API Version constant automagically
+func (r *LocalRouter) HandleFuncVersioned(version, path string, f func(http.ResponseWriter, *http.Request)) *mux.Route {
+	return r.HandleFunc("/"+version+path, f)
+}
+
+func setRoutes(router *LocalRouter) {
+	//Setup Routes In Here
+	//Example: SetContactUsRoutes()
+}
+
+/* Example:
+func SetContactUsRoutes() {
+	//Regex For If We Are Expecting an ID in the URL
+	mysqlIdRegex := "(?:[0-9]{1,24})"
+
+	expectedUrl := "/contact/")
+	expectedUrlWithRegex := fmt.Sprintf("/contact/{contactID:%s}/")
+	router.HandleFuncVersioned(
+		API_VERSION,
+		expectedUrl,
+		contactHandler,).Methods("PUT", "GET").Name("contact")
+	)
+
+}
+
+*/
