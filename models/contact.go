d7df63b8afd9387686cea9175f4f1f285f9a03b6
Julian Dasilva <julianalecssandredasilva@gmail.com>
2020-07-29 22:12:53 -0400

    Add a method to select contacts from the mysql db to the contact struct

--- models/contact.go
+++ models/contact.go
@@ -1,7 +1,35 @@
 package models
 
+import (
+	"go_server/db"
+	"go_server/log"
+)
+
 type Contact struct {
 	Name    string `db:"name" json:"name"`
 	Email   string `db:"email" json:"email"`
 	Message string `db:"message" json:"message"`
+}
+
+func (c Contact) SelectAllContacts() ([]*Contact, error) {
+	query := "SELECT c.name, c.email, c.message FROM contact c"
+	rows, err := db.Db().DB.Queryx(query)
+	if err != nil {
+		log.Errorf("Failed to Retrieve Contacts From Database: %v", err)
+		return nil, err
+	}
+	defer rows.Close()
+	var selectedContacts []*Contact
+
+	for rows.Next() {
+		contact := new(Contact)
+		err := rows.StructScan(contact)
+		if err != nil {
+			log.Errorf("Failed to scan rows into contact struct: %v", err)
+			return nil, err
+		}
+		selectedContacts = append(selectedContacts, contact)
+	}
+
+	return selectedContacts, nil
 }
